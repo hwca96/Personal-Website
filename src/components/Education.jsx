9eb0cdfdb70a3f8739011b7c0ae0f5b6a684bdac
Harvey Wu <hwca96@gmail.com>
2023-08-05 15:31:02 -0700

    added online image

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,7 +7,7 @@ function Education() {
       <Row className="justify-content-md-center">
         <Col className="mx-0 flex-grow-0">
           <Card style={{ width: "20rem" }} className="mx-1">
-            <Card.Img variant="top" src="src\assets\ubc-logo.png" />
+            <Card.Img variant="top" src="https://pbs.twimg.com/profile_images/1174018931532550144/jRmFjhVX_400x400.png" />
             <Card.Body>
               <Card.Title>Bachelor of Computer Science</Card.Title>
               <Card.Text>
@@ -24,7 +24,7 @@ function Education() {
         </Col>
         <Col className="mx-0 flex-grow-0">
           <Card style={{ width: "20rem" }} className="mx-1">
-            <Card.Img variant="top" src="src\assets\ubc-logo.png" />
+            <Card.Img variant="top" src="https://pbs.twimg.com/profile_images/1174018931532550144/jRmFjhVX_400x400.png" />
             <Card.Body>
               <Card.Title>Bachelor of Science (Chemistry)</Card.Title>
               <Card.Text>
