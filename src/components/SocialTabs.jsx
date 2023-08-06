3116540ad66cc550319c4dc60dd52dc3bed7ff18
Harvey Wu <hwca96@gmail.com>
2023-08-05 19:17:16 -0700

    added links to social tabs

--- src/components/SocialTabs.jsx
+++ src/components/SocialTabs.jsx
@@ -5,13 +5,13 @@ function SocialTabs() {
               rel="stylesheet"
               href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
             />
-          <a href="#" className="linkedin">
+          <a href="https://www.linkedin.com/in/wu-harvey" target="_blank" className="linkedin">
             <i className="fa fa-linkedin"></i>
           </a>
-          <a href="#" className="github">
+          <a href="https://github.com/hwca96" target="_blank" className="github">
             <i className="fa fa-github"></i>
           </a>
-          <a href="#" className="email">
+          <a href="mailto:hwca96@gmail.com" target="_blank" className="email">
             <i className="fa fa-envelope"></i>
           </a>
         </div>
