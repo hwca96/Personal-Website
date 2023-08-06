ea6ea37e8f7c50b0490de14a4f7e37be8ff2272b
Harvey Wu <hwca96@gmail.com>
2023-08-05 19:08:02 -0700

    added icon

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,16 +7,18 @@ function Education() {
       <Row className="justify-content-md-center">
         <Col className="mx-0 flex-grow-0">
           <Card style={{ width: "20rem" }} className="mx-1">
-            <Card.Img variant="top" src="https://pbs.twimg.com/profile_images/1174018931532550144/jRmFjhVX_400x400.png" />
+            <Card.Img
+              variant="top"
+              src="https://pbs.twimg.com/profile_images/1174018931532550144/jRmFjhVX_400x400.png"
+            />
             <Card.Body>
               <Card.Title>Bachelor of Computer Science</Card.Title>
               <Card.Text>
-                <p className="uni-name">The University of British Columbia</p>
-                <p>
+                  The University of British Columbia
+                  <br />
                   September 2021 - Current
                   <br />
                   (Expected Graduation: December 2023)
-                </p>
               </Card.Text>
               <Button variant="primary">Notable Courses</Button>
             </Card.Body>
@@ -24,14 +26,17 @@ function Education() {
         </Col>
         <Col className="mx-0 flex-grow-0">
           <Card style={{ width: "20rem" }} className="mx-1">
-            <Card.Img variant="top" src="https://pbs.twimg.com/profile_images/1174018931532550144/jRmFjhVX_400x400.png" />
+            <Card.Img
+              variant="top"
+              src="https://pbs.twimg.com/profile_images/1174018931532550144/jRmFjhVX_400x400.png"
+            />
             <Card.Body>
               <Card.Title>Bachelor of Science (Chemistry)</Card.Title>
               <Card.Text>
-                <p className="uni-name">The University of British Columbia</p>
-                <p>
-                  September 2015 - May 2020 <br /> Graduated with Co-op Designation
-                </p>
+                  The University of British Columbia
+                  <br />
+                  September 2015 - May 2020 <br /> Graduated with Co-op
+                  Designation
               </Card.Text>
               <Button variant="primary">Notable Courses</Button>
             </Card.Body>
